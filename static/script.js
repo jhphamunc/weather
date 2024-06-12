c88e0c6d0f61bf1bec5964070f91404623a4e3a4
Joseph Pham <josephhpham751@gmail.com>
2024-06-12 16:29:50 -0400

    added if statements

--- static/script.js
+++ static/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $('#add-location-form-longitude').submit(function(e){
         e.preventDefault();
         var newLocationLongitude = $('#new-location-longitude').val();
-        if (!isNaN(newLocationLongitude)) {
+        if (!isNaN(newLocationLongitude) && newLocationLongitude >= 0 && newLocationLongitude <= 90) {
             // Input is a number
             console.log("Longitude:", newLocationLongitude);
         } else {
@@ -15,12 +15,12 @@ $(document).ready(function () {
     $('#add-location-form-latitude').submit(function(e){
         e.preventDefault();
         var newLocationLatitude = $('#new-location-latitude').val();
-    if (!isNaN(newLocationLatitude)) {
-        // Input is a number
-        console.log("Latitude:", newLocationLatitude);
-    } else {
-        // Input is not a number
-        console.log("Invalid input for latitude:", newLocationLatitude);
-    }
+        if (!isNaN(newLocationLatitude) && newLocationLatitude >= 0 && newLocationLatitude <= 90) {
+            // Input is a number
+            console.log("Latitude:", newLocationLatitude);
+        } else {
+            // Input is not a number
+            console.log("Invalid input for latitude:", newLocationLatitude);
+        }
     })
 })
